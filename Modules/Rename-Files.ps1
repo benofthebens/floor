0899320fe0f97d91f20d8b0e367d4b488452de5c
benjamin whalley <benjaminjwhalley@outlook.com>
2024-02-08 00:10:39 +0000

    chore: Rename-Files module comments added

--- Modules/Rename-Files.ps1
+++ Modules/Rename-Files.ps1
@@ -1,18 +1,21 @@
 . "C:\Users\benja\Desktop\Floor\Scripts\Public\File.ps1"
 
 function Rename-Files(){
+     #get the children of the current folder
      $files = Get-ChildItem -File
+     #get json path
      $jsonFilePath = "C:\Users\benja\Desktop\Floor\Resources\data\Renamed_Files.json"
      # Read the existing JSON content
      $jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
 
      
      foreach($file in $files){
-
+          #initalise new file object
           $newFile = [File]::new($file.BaseName,$file.CreationTime.Date.ToString("yyyy_MM_dd"),"DOT",0,$file.Extension)
-          
+          #new path
           $newFilePath = Join-Path -Path $file.Directory.FullName -ChildPath $newFile.ToString()
-
+          #if the same file exsits increment the version 
+          #(subject to change)
           while($jsonContent.Files -contains $newFilePath){
                $newFile.IncrementVersion()
                $newFilePath = Join-Path -Path $file.Directory.FullName -ChildPath $newFile.ToString()
@@ -22,6 +25,7 @@ function Rename-Files(){
                Write-Host "File path already exists in the JSON file: $newFilePath"
           }
           else {
+               #renames the file 
                Rename-Item -Path $file.FullName -NewName $newFile.ToString()
 
                # Add the new file path to the JSON content
